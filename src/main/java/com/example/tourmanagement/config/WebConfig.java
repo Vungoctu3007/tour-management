0225b4e402dd0fa1111643dd225422e8e82725a3
Tu <vungoctu12a3@gmail.com>
2024-09-24 20:00:16 +0700

    update

--- src/main/java/com/example/tourmanagement/config/WebConfig.java
+++ src/main/java/com/example/tourmanagement/config/WebConfig.java
@@ -1,4 +1,17 @@
 package com.example.tourmanagement.config;
 
-public class config {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // Adjust this URL to match your frontend's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 }
