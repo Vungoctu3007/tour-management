6581401f3b596d48f2c14ba785f0917114a3c880
truongly2003 <118749801+truongly2003@users.noreply.github.com>
2024-10-26 11:17:29 +0700

    update

--- src/main/java/com/example/tourmanagement/controller/TourController.java
+++ src/main/java/com/example/tourmanagement/controller/TourController.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api/tour")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
