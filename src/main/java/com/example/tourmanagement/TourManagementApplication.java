2adaddf504e962f2286ad0ce5fbd605e75e8ae24
Tuan Nguyen <116452243+hoangtuan203@users.noreply.github.com>
2024-11-04 20:50:12 +0700

    update register

--- src/main/java/com/example/tourmanagement/TourManagementApplication.java
+++ src/main/java/com/example/tourmanagement/TourManagementApplication.java
@@ -1,11 +1,15 @@
 package com.example.tourmanagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"com.example.tourmanagement"})
 public class TourManagementApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("JWT_SIGNER_KEY", dotenv.get("JWT_SIGNER_KEY"));
 		SpringApplication.run(TourManagementApplication.class, args);
 	}
 
