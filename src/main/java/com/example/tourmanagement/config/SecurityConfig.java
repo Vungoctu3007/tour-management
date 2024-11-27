a909be9b0a569bbc6fd3443c5ff894a133f7d8c7
truongly2003 <118749801+truongly2003@users.noreply.github.com>
2024-11-27 14:25:00 +0700

    update

--- src/main/java/com/example/tourmanagement/config/SecurityConfig.java
+++ src/main/java/com/example/tourmanagement/config/SecurityConfig.java
@@ -32,86 +32,73 @@ public class SecurityConfig {
   @Autowired
   MyAuthorizationManager myAuthorizationManager;
 
-<<<<<<< HEAD
-  private final String[] PUBLIC_ENDPOINT = {"/api/auth/**", "/api/route/**",
-      "/api/admin/feedback/client", "/api/booking/**"};
-  private final String[] PRIVATE_ENDPOINT = {"/api/admin/user/**", "/api/admin/role/**",
-      "/api/admin/decentralization/**", "/api/admin/feedback/admin", "/api/admin/customer/**"};
-=======
-    private final String[] PUBLIC_ENDPOINT = {"/api/auth/**", "/api/route/**", "/api/admin/feedback/client", "/api/customer/**", "/api/booking/**", "/api/vnpay/**"};
-    private final String[] PRIVATE_ENDPOINT = {"/api/admin/user/**", "/api/admin/role/**", "/api/admin/decentralization/**", "/api/admin/feedback/admin"};
+
+  private final String[] PUBLIC_ENDPOINT = {"/api/auth/**", "/api/route/**", "/api/admin/feedback/client", "/api/customer/**", "/api/booking/**", "/api/vnpay/**"};
+  private final String[] PRIVATE_ENDPOINT = {"/api/admin/user/**", "/api/admin/role/**", "/api/admin/decentralization/**", "/api/admin/feedback/admin"};
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity, MyAuthorizationManager myAuthorizationManager) throws Exception {
-        httpSecurity
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Thêm CORS vào HttpSecurity
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.OPTIONS, PUBLIC_ENDPOINT).permitAll()
-                        .requestMatchers(PUBLIC_ENDPOINT).permitAll()
->>>>>>> 62ccf8ebdee9280cf08a67da72582d52697910a8
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
+            MyAuthorizationManager myAuthorizationManager) throws Exception {
+      httpSecurity
+              .cors(cors -> cors.configurationSource(
+                      corsConfigurationSource())) // Thêm CORS vào HttpSecurity
+              .authorizeHttpRequests(request -> request
+                      .requestMatchers(PUBLIC_ENDPOINT).permitAll() // Cho phép truy cập không cần xác thực
+                      .requestMatchers(PRIVATE_ENDPOINT).hasAnyRole("STAFF", "ADMIN")
+                      .anyRequest().authenticated()
+              )
+              .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
+                      .decoder(customJwtDecoder)
+                      .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+              )
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
-      MyAuthorizationManager myAuthorizationManager) throws Exception {
-    httpSecurity
-        .cors(cors -> cors.configurationSource(
-            corsConfigurationSource())) // Thêm CORS vào HttpSecurity
-        .authorizeHttpRequests(request -> request
-            .requestMatchers(PUBLIC_ENDPOINT).permitAll() // Cho phép truy cập không cần xác thực
-            .requestMatchers(PRIVATE_ENDPOINT).hasAnyRole("STAFF", "ADMIN")
-            .anyRequest().authenticated()
-        )
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer
-            .decoder(customJwtDecoder)
-            .jwtAuthenticationConverter(jwtAuthenticationConverter()))
-        )
+              .csrf(AbstractHttpConfigurer::disable); // Tắt CSRF vì sử dụng JWT
 
-        .csrf(AbstractHttpConfigurer::disable); // Tắt CSRF vì sử dụng JWT
+      return httpSecurity.build();
+    }
 
-    return httpSecurity.build();
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+      CorsConfiguration corsConfiguration = new CorsConfiguration();
+      corsConfiguration.addAllowedOrigin("http://localhost:3000"); // Cho phép React truy cập
+      corsConfiguration.addAllowedMethod("*"); // Cho phép tất cả các phương thức HTTP
+      corsConfiguration.addAllowedHeader("*"); // Cho phép tất cả các header
+      corsConfiguration.setAllowCredentials(true); // Cho phép gửi cookie hoặc credential
+
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", corsConfiguration);
+      return source;
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+      JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+      jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_"); // Không thêm tiền tố
+      jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("scope"); // Sử dụng trường "scope"
+
+      jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("scope");
+
+      JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+      jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+        // Lấy claim "scope"
+        String rawScope = jwt.getClaim("scope");
+
+        if (rawScope != null) {
+          rawScope = rawScope.replace("\r", "").replace("\n", "")
+                  .trim(); // Loại bỏ \r\n và khoảng trắng
+          return List.of(new SimpleGrantedAuthority(rawScope));
+        }
+
+        return List.of();
+      });
+
+      return jwtAuthenticationConverter;
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder(10);
+    }
   }
-
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.addAllowedOrigin("http://localhost:3000"); // Cho phép React truy cập
-    corsConfiguration.addAllowedMethod("*"); // Cho phép tất cả các phương thức HTTP
-    corsConfiguration.addAllowedHeader("*"); // Cho phép tất cả các header
-    corsConfiguration.setAllowCredentials(true); // Cho phép gửi cookie hoặc credential
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", corsConfiguration);
-    return source;
-  }
-
-  @Bean
-  public JwtAuthenticationConverter jwtAuthenticationConverter() {
-    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-    jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_"); // Không thêm tiền tố
-    jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("scope"); // Sử dụng trường "scope"
-
-    jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("scope");
-
-    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
-      // Lấy claim "scope"
-      String rawScope = jwt.getClaim("scope");
-
-      if (rawScope != null) {
-        rawScope = rawScope.replace("\r", "").replace("\n", "")
-            .trim(); // Loại bỏ \r\n và khoảng trắng
-        return List.of(new SimpleGrantedAuthority(rawScope));
-      }
-
-      return List.of();
-    });
-
-    return jwtAuthenticationConverter;
-  }
-
-
-  @Bean
-  PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder(10);
-  }
-}
